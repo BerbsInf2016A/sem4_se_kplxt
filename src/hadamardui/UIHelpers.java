920ca352dd8ff7e7853aa639ae5f8e5ec7533157
Berberich André <berbs1989@googlemail.com>
2018-03-13 18:04:12 +0100

    Removed unused code

--- src/hadamardui/UIHelpers.java
+++ src/hadamardui/UIHelpers.java
@@ -17,44 +17,6 @@ import static hadamardui.UIConfiguration.*;
 
 public class UIHelpers {
 
-
-
-
-    public static GridPane generateMatrix(Matrix changedMatrix) {
-        GridPane root = new GridPane();
-        int dimension = changedMatrix.getDimension();
-
-        for (int row = 0; row < dimension; row++){
-            for (int column = 0; column < dimension; column++){
-                Rectangle rec = new Rectangle();
-                rec.widthProperty().bind(root.widthProperty().divide(dimension));
-                rec.heightProperty().bind(root.heightProperty().divide(dimension));
-                rec.setStroke(Color.GRAY);
-                rec.setStrokeType(StrokeType.INSIDE);
-
-
-
-
-
-                CellValue value = getCellValue(row, column, changedMatrix);
-                switch (value) {
-                    case Positive:
-                        rec.setFill(Color.BLACK);
-                        break;
-                    case Negative:
-                        rec.setFill(Color.WHITE);
-                        break;
-                    case Unset:
-                        rec.setFill(Color.PINK);
-                        break;
-                }
-                root.add(rec, column, row);
-            }
-        }
-
-        return root;
-    }
-
     private static void drawRectangle(GraphicsContext gc,Rectangle rect, Color fillColor){
         gc.setFill(Color.GRAY);
         gc.fillRect(rect.getX(),
@@ -62,7 +24,6 @@ public class UIHelpers {
                 rect.getWidth(),
                 rect.getHeight());
         gc.setFill(fillColor);
-        //gc.setStroke(Color.GRAY);
         gc.fillRect(rect.getX(),
                 rect.getY(),
                 rect.getWidth() - 2,
@@ -114,37 +75,6 @@ public class UIHelpers {
         return value ? CellValue.Positive : CellValue.Negative;
     }
 
-    public static void updateTabContent(GridPane content, int columnIndex, BitSet column) {
-        GridPane root = content;
-        for (int row = 0; row < Configuration.instance.dimension; row++){
-            Rectangle rec =  (Rectangle)(root.getChildren().get(row* Configuration.instance.dimension+ columnIndex));
-            updateValue(rec, row, column);
-        }
-/*
-        for (Node node : root.getChildren()) {
-            if (GridPane.getColumnIndex(node) == columnIndex){
-                Rectangle rec = (Rectangle) node;
-                updateValue(rec, GridPane.getRowIndex(node), column);
-            }
-        }
-        */
-    }
-
-    private static void updateValue(Rectangle rec, Integer rowIndex, BitSet column) {
-        CellValue value = getCellValue(rowIndex, column);
-        switch (value) {
-            case Positive:
-                rec.setFill(Color.BLACK);
-                break;
-            case Negative:
-                rec.setFill(Color.WHITE);
-                break;
-            case Unset:
-                rec.setFill(Color.PINK);
-                break;
-        }
-
-    }
 
     private static CellValue getCellValue(Integer rowIndex, BitSet column) {
         boolean value = column.get(rowIndex);
